2eebc7371c495ddeb4cb0c29311fd30b70227552
Lancy <lancy@dago-bpo.com>
2018-07-26 11:41:05 +0300

    010101

--- finances-hospitalieres/js/application.js
+++ finances-hospitalieres/js/application.js
@@ -1,36 +1,3 @@
 $(document).ready(function(){
 
-    $(".select_annee").select2({
-        placeholder: "Année",
-        allowClear: true
-    });
-    $(".select_mois").select2({
-        placeholder: "Mois",
-        allowClear: true
-    });
-    $(".select_parution").select2({
-        placeholder: "Numéro de parution",
-        allowClear: true
-    });
-
-    $(".select_rubrique").select2({
-        placeholder: "Rubrique",
-        allowClear: true
-    });
-
-    $(".select_auteur").select2({
-        placeholder: "Auteur",
-        allowClear: true
-    });
-
-    $(".select_index").select2({
-        placeholder: "Index",
-        allowClear: true
-    });
-
-    $(".select_decision").select2({
-        placeholder: "Type de décision",
-        allowClear: true
-    });
-
 });
